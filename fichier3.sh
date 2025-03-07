8a3f86db851333f660f914f6703dab1ed8a24f8b
kadirdolek <kadirdolek@outlook.com>
2025-03-07 10:52:22 +0100

    exo3

---
+++ fichier3.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+mkdir -p public/{css/{footer,header,main},img,js} && touch public/css/{footer,header,main}/styles.css public/index.html && touch public/js/main.js && touch public/img/{img1,img2}
+cmd //c tree
